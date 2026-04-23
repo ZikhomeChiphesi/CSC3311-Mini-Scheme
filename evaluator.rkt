606d28e2888ceb6930d608797076302c1aac3722
Zikhome Chiphesi <zikhomechip@gmail.com>
2026-04-23 02:48:45 +0200

    Complete Task 3: Ribcage environment system and evaluator fixes

--- evaluator.rkt
+++ evaluator.rkt
@@ -1,9 +1,14 @@
 #lang sicp
 
+;; =========================
+;; BASIC EXPRESSION TYPES
+;; =========================
+
 (define (self-evaluating? exp)
   (or (number? exp)
       (string? exp)
       (boolean? exp)))
+
 (define false #f)
 
 (define (variable? exp)
@@ -33,93 +38,17 @@
 (define (application? exp)
   (pair? exp))
 
-(define (operator exp)
-  (car exp))
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
 
-(define (operands exp)
-  (cdr exp))
+(define (lambda-parameters exp) (cadr exp))
+(define (lambda-body exp) (cddr exp))
 
-(define (lambda-parameters exp)
-  (cadr exp))
+(define (begin-actions exp) (cdr exp))
 
-(define (lambda-body exp)
-  (cddr exp))
-
-(define (begin-actions exp)
-  (cdr exp))
-
-(define (lookup-variable-value var env)
-  (cond ((null? env)
-         (error "Unbound variable" var))
-        (else
-         (let ((frame (car env)))
-           (let ((binding (assoc var frame)))
-             (if binding
-                 (cdr binding)
-                 (lookup-variable-value var (cdr env))))))))
-
-(define (assignment-variable exp)
-  (cadr exp))
-
-(define (assignment-value exp)
-  (caddr exp))
-
-(define (eval-assignment exp env)
-  (set-variable-value!
-   (assignment-variable exp)
-   (mini-eval (assignment-value exp) env)
-   env)
-  'ok)
-
-(define (set-variable-value! var val env)
-  (cond ((null? env)
-         (error "Unbound variable -- SET!" var))
-        (else
-         (let ((frame (car env)))
-           (let ((binding (assoc var frame)))
-             (if binding
-                 (set-cdr! binding val)
-                 (set-variable-value! var val (cdr env))))))))
-
-(define (definition-variable exp)
-  (cadr exp))
-
-(define (definition-value exp)
-  (caddr exp))
-
-(define (eval-definition exp env)
-  (define-variable!
-   (definition-variable exp)
-   (mini-eval (definition-value exp) env)
-   env)
-  'ok)
-
-(define (define-variable! var val env)
-  (let ((frame (car env)))
-    (let ((binding (assoc var frame)))
-      (if binding
-          (set-cdr! binding val)
-          (set-car! env (cons (cons var val) frame))))))
-
-(define (if-predicate exp)
-  (cadr exp))
-
-(define (if-consequent exp)
-  (caddr exp))
-
-(define (if-alternative exp)
-  (if (not (null? (cdddr exp)))
-      (cadddr exp)
-      'false))
-
-(define (eval-if exp env)
-  (if (true? (mini-eval (if-predicate exp) env))
-      (mini-eval (if-consequent exp) env)
-      (mini-eval (if-alternative exp) env)))
-
-(define (true? x)
-  (not (eq? x false)))
-
+;; =========================
+;; EVALUATOR CORE
+;; =========================
 
 (define (mini-eval exp env)
   (cond ((self-evaluating? exp) exp)
@@ -145,6 +74,10 @@
       (cons (mini-eval (car exps) env)
             (list-of-values (cdr exps) env))))
 
+;; =========================
+;; PROCEDURE APPLICATION
+;; =========================
+
 (define (mini-apply procedure arguments)
   (cond ((primitive-procedure? procedure)
          (apply-primitive-procedure procedure arguments))
@@ -158,54 +91,148 @@
         (else
          (error "Unknown procedure type -- APPLY" procedure))))
 
+;; =========================
+;; PROCEDURES
+;; =========================
+
 (define (make-procedure parameters body env)
   (list 'procedure parameters body env))
 
 (define (compound-procedure? p)
   (tagged-list? p 'procedure))
 
-(define (procedure-parameters p)
-  (cadr p))
+(define (procedure-parameters p) (cadr p))
+(define (procedure-body p) (caddr p))
+(define (procedure-environment p) (cadddr p))
 
-(define (procedure-body p)
-  (caddr p))
+;; =========================
+;; PRIMITIVES
+;; =========================
 
-(define (procedure-environment p)
-  (cadddr p))
+(define (make-primitive name proc)
+  (list 'primitive name proc))
 
 (define primitive-procedures
   (list
-   (list 'car car)
-   (list 'cdr cdr)
-   (list 'cons cons)
-   (list 'null? null?)
-   (list '+ +)
-   (list '- -)
-   (list '* *)
-   (list '/ /)))
+   (make-primitive '+ +)
+   (make-primitive '- -)
+   (make-primitive '* *)
+   (make-primitive '/ /)))
 
 (define (primitive-procedure? proc)
   (tagged-list? proc 'primitive))
 
-(define (primitive-implementation proc)
-  (cadr proc))
-
 (define (apply-primitive-procedure proc args)
-  (apply (primitive-implementation proc) args))
+  (apply (caddr proc) args))
 
-(define (setup-primitives)
-  (map (lambda (p)
-         (list 'primitive (cadr p)))
-       primitive-procedures))
+;; =========================
+;; ENVIRONMENT (RIBCAGE)
+;; =========================
 
-(define (tagged-list? exp tag)
-  (and (pair? exp)
-       (eq? (car exp) tag)))
+(define (make-frame vars vals)
+  (cons vars vals))
 
 (define (extend-environment vars vals base-env)
   (if (= (length vars) (length vals))
-      (cons (map cons vars vals) base-env)
-      (error "Incorrect number of arguments supplied")))
+      (cons (make-frame vars vals) base-env)
+      (error "Arguments mismatch -- EXTEND-ENVIRONMENT" vars vals)))
+
+(define (frame-variables frame)
+  (car frame))
+
+(define (frame-values frame)
+  (cdr frame))
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (if (null? env)
+        (error "Unbound variable" var)
+        (scan (frame-variables (car env))
+              (frame-values (car env))
+              (cdr env))))
+
+  (define (scan vars vals rest-env)
+    (cond ((null? vars)
+           (env-loop rest-env))
+          ((eq? var (car vars))
+           (car vals))
+          (else
+           (scan (cdr vars) (cdr vals) rest-env))))
+
+  (env-loop env))
+
+;; =========================
+;; ASSIGNMENT / DEFINITION
+;; =========================
+
+(define (assignment-variable exp) (cadr exp))
+(define (assignment-value exp) (caddr exp))
+
+(define (eval-assignment exp env)
+  (set-variable-value!
+   (assignment-variable exp)
+   (mini-eval (assignment-value exp) env)
+   env)
+  'ok)
+
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (if (null? env)
+        (error "Unbound variable -- SET!" var)
+        (scan (frame-variables (car env))
+              (frame-values (car env))
+              (cdr env))))
+
+  (define (scan vars vals rest-env)
+    (cond ((null? vars)
+           (env-loop rest-env))
+          ((eq? var (car vars))
+           (set-car! vals val))
+          (else
+           (scan (cdr vars) (cdr vals) rest-env))))
+
+  (env-loop env))
+
+(define (definition-variable exp) (cadr exp))
+(define (definition-value exp) (caddr exp))
+
+(define (eval-definition exp env)
+  (define-variable!
+   (definition-variable exp)
+   (mini-eval (definition-value exp) env)
+   env)
+  'ok)
+
+(define (define-variable! var val env)
+  (let ((frame (car env)))
+    (let loop ((vars (car frame))
+               (vals (cdr frame)))
+      (cond ((null? vars)
+             (set-car! frame (cons var (car frame)))
+             (set-cdr! frame (cons val (cdr frame))))
+            ((eq? var (car vars))
+             (set-car! vals val))
+            (else
+             (loop (cdr vars) (cdr vals)))))))
+
+;; =========================
+;; IF + SEQUENCES
+;; =========================
+
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (eval-if exp env)
+  (if (true? (mini-eval (if-predicate exp) env))
+      (mini-eval (if-consequent exp) env)
+      (mini-eval (if-alternative exp) env)))
+
+(define (true? x)
+  (not (eq? x false)))
 
 (define (eval-sequence exps env)
   (cond ((last-exp? exps)
@@ -218,13 +245,27 @@
 (define (rest-exps seq) (cdr seq))
 (define (last-exp? seq) (null? (cdr seq)))
 
+;; =========================
+;; ENV HELPERS
+;; =========================
+
+(define (tagged-list? exp tag)
+  (and (pair? exp)
+       (eq? (car exp) tag)))
+
+;; =========================
+;; GLOBAL ENVIRONMENT
+;; =========================
+
 (define the-global-environment
   (list
-   (list
-    (cons '+ (list 'primitive +))
-    (cons '- (list 'primitive -))
-    (cons '* (list 'primitive *))
-    (cons '/ (list 'primitive /)))))
+   (make-frame
+    (map (lambda (p) (cadr p)) primitive-procedures)
+    primitive-procedures)))
+
+;; =========================
+;; TESTS
+;; =========================
 
 (mini-eval '(+ 2 3) the-global-environment)
 (mini-eval '(* 4 5) the-global-environment)
